22117c697bf31a0d58c71d0da68a352ee33383ef
gennaroderosa10 <gennaroderosa5@gmail.com>
2025-12-03 17:54:51 +0100

    feat: milestone 1 done, stampate attrici in console

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
+import { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [attrici, setAttrici] = useState([])
+
+  useEffect(() => {
+    fetchAttrici();
+  }, []);
+
+  function fetchAttrici() {
+    axios
+      .get(`https://lanciweb.github.io/demo/api/actresses/`)
+      .then((resp) => {
+        setAttrici(resp.data)
+        console.log(resp.data)
+      })
+  }
+
+
 
   return (
     <>
+      <header>
+        <nav className="navbar bg-body-tertiary">
+          <h1>
+            ATTRICI
+
+          </h1>
+        </nav>
+      </header>
+
 
     </>
   )
