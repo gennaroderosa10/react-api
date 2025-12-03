f356a1b671fd1ec07760bec792dafe7ce0f7bded
gennaroderosa10 <gennaroderosa5@gmail.com>
2025-12-03 18:20:50 +0100

    feat: create tutte le card per ogni attrice

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,37 @@ function App() {
         </nav>
       </header>
 
+      <main className='container mt-4'>
+        <div className="row">
+
+          {attrici.map((attrice, index) => {
+            return (
+              <div className="card col-3 g-3" key={index}>
+                <img src={attrice.image} className="card-img-top" alt={attrice.name} />
+                <div className="card-body">
+                  <h5 className="card-title">{attrice.name}</h5>
+                  <p className="card-text">
+                    {attrice.biography}
+                  </p>
+                </div>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item">{attrice.birth_year}</li>
+                  <li className="list-group-item">{attrice.nationality}</li>
+                  <li className="list-group-item">{attrice.awards}</li>
+                </ul>
+              </div>
+            );
+          })}
+
+
+
+
+        </div>
+
+
+
+      </main>
+
 
     </>
   )
